16d8e39f331aa84eb2eb91cc9847e9223849e6f5
chirag.jain <chirag.jain@intimetec.com>
2023-07-21 19:55:18 +0530

    production

---
+++ src/environments/enviornment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAWNxR35qugxqqEDLSDxDdJdigrHfH4x4',
+  authDomain: 'unstop-assessment.firebaseapp.com',
+  projectId: 'unstop-assessment',
+  storageBucket: 'unstop-assessment.appspot.com',
+  messagingSenderId: '84695040884',
+  appId: '1:84695040884:web:46cf6ecb0348b48fb5a221',
+  measurementId: 'G-66VFJ1F906',
+};
